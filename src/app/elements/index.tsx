f0c0d64f5895983ceacac4ad91c370ec66f4f35a
Raúl <raulacostadeveloper@outlook.com>
2023-12-06 16:24:49 -0600

    Ejemplos

--- src/app/elements/index.tsx
+++ src/app/elements/index.tsx
@@ -4,6 +4,12 @@ import { ElementoEnLista } from "./ElementoEnLista/ElementoEnLista"
 export const Index = ():JSX.Element => {
     return (
         <div>
+            <p>Los tipos de elementos son </p>
+            <p>1._prioridad/fecha: Pueden ordenarse por la fecha o por el número de prioridad que tenga (uno u otro)</p>
+            <p>2._ Tipo texto puede ser usado como título o como nota</p>
+            <p>3._ Cantidad puede ser tomado como dinero, tiempo, o cualquier otro elemento contable</p>
+            <ElementoEnLista/>
+            <ElementoEnLista/>
             <ElementoEnLista/>
         </div>
     )
